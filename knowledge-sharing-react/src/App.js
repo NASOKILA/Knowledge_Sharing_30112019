77e25ad3efdc8bf648f2abf40d93e7725674fc3e
Atanas <atanasskambitovv@gmail.com>
2019-11-29 06:10:09 +0200

    Make the form work by adding new tweet. Make removing, liking and disliking buttons work.

--- knowledge-sharing-react/src/App.js
+++ knowledge-sharing-react/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { TweetList } from "./components/TweetList/TweetList";
+import TweetForm from "./components/TweetForm/TweetForm";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tweets: []
+    };
+  }
+
+  //unmounting
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  //Updating
+  componentWillUpdate() {
+    console.log("componentWillUpdate");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  //Mounting
+  componentWillMount() {
+    console.log("componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    //API calls
+    setTimeout(() => {
+      let tweets = [
+        { id: 1, name: "Tweet1", content: "Some text..." },
+        { id: 2, name: "Tweet2", content: "Some text..." }
+      ];
+      this.setState({ tweets });
+    }, 1000);
+  }
+
+  addTweet = tweet => {
+    let tweets = [...this.state.tweets];
+    tweets.push(tweet);
+    this.setState({ tweets });
+  };
+
+  removeTweet = id => {
+    console.log(id);
+    let tweets = this.state.tweets.filter(tw => tw.id !== id);
+    this.setState({ tweets });
+  };
+
+  render() {
+    console.log("render");
+    return (
+      <div className="App">
+        <h1>React Tweets</h1>
+        <TweetList removeTweet={this.removeTweet}>
+          {this.state.tweets}
+        </TweetList>
+        <br />
+        <TweetForm
+          addTweet={this.addTweet}
+          tweetsCount={this.state.tweets.length}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
